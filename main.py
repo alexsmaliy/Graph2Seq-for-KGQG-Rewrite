a1cce6b9aedbb4188ded70d1374c2c95ceebb1dd
Alex Smaliy <alex.smaliy@gmail.com>
2024-05-01 12:02:05 -0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import config
+import numpy as np
+import random
+import torch
+
+
+def set_random_seed_everywhere(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+
 
 def main():
-    random_seed = config.RANDOM_SEED
+    set_random_seed_everywhere(config.RANDOM_SEED)
+    print("moo")
 
-if __name__ == "main":
+
+if __name__ == "__main__":
     main()
